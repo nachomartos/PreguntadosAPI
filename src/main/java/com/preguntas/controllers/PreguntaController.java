239da72f059146ca437ad3e84bd4d20296511e08
nachomartos <nachomartos@hotmail.com>
2024-10-02 20:55:23 -0300

    Update PreguntaController.java

--- src/main/java/com/preguntas/controllers/PreguntaController.java
+++ src/main/java/com/preguntas/controllers/PreguntaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping("api/v1/preguntas")
 public class PreguntaController {
 
